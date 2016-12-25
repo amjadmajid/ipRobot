5fa7f7ef7c7906b17112be7da2c9460d0aa57ddf
Koen Schaper <Kpschaper@gmail.com>
2016-12-25 13:58:53 +0100

    Implementation of i2c expander motor driver

--- Motor-driver/main.c
+++ Motor-driver/main.c
@@ -6,10 +6,9 @@
 #include <msp430.h>
 #include <stdint.h>
 
+#define DATA_SIZE 2
+#define SLAVE_ADDR 116 // TCA9539 address pins to GND
 #define NUM_OF_STATES 4
-#define DUTY_CYCLE 4
-#define FREQUENCY 40
-
 
 void init(void) {
 
@@ -19,45 +18,81 @@ void init(void) {
     CSCTL0_H = CSKEY_H;                     // Unlock clock registers
     CSCTL1 = DCOFSEL_3 | DCORSEL;           // Set DCO to 8MHz
     CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
-    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers
+    CSCTL3 = DIVA__1 | DIVS__8 | DIVM__1;   // Set all dividers
     CSCTL0_H = 0;                           // Lock CS registers
 
-    P1DIR |= BIT0;							// Set led2 pin to output
-    //P4DIR |= BIT6;						// Set led1 pin to output
+    P1DIR |= BIT0;                          // Set led2 pin to output
 
-    P1SEL0 &= ~0x78;						// Set right drv8836 pins to general i/o (pin 3, 4, 5, 6)
-    P1DIR |= 0x78;							// Set right drv8836 pins to output
+    P1DIR |= BIT4;                          // Set P1.4 (AUX3) to output
+    P1OUT &= ~BIT4;                         // Hold TCA9539 in reset (active low)
+}
 
-    P3SEL0 &= ~0x71;						// Set left drv8836 pins to general i/o (pin 0, 4, 5, 6)
-    P3DIR |= 0x71;							// Set left drv8836 pins to output
+void i2c_init(){
+
+    // I2C default uses SMCLK
+    UCB0CTL1 |= UCSWRST;                    // put eUSCI_B in reset state
+    UCB0CTLW0 |= UCMODE_3 | UCMST | UCSYNC; // I2C, master, sync
+    UCB0BRW = 0x000A;                       // baud rate = SMCLK / 10 = 100khz
+    UCB0CTLW1 = UCASTP_2;                   // automatic STOP assertion
+    UCB0TBCNT = DATA_SIZE;                  // TX 2 bytes of data
+    UCB0I2CSA = SLAVE_ADDR;                 // slave address
+
+    P1SEL1 |= BIT6 | BIT7;                  // configure I2C pins
+    P1SEL0 &= ~(BIT6 | BIT6);               // configure I2C pins
+
+    UCB0CTL1 &= ~UCSWRST;                   // eUSCI_B in operational state
+}
+
+void i2c_transmit(uint8_t cmd, uint8_t data){
+
+    UCB0CTLW0 |= UCTR + UCTXSTT;            // transmitter mode and START condition.
+
+    while(!(UCB0IFG & UCTXIFG0));
+    UCB0TXBUF = cmd;
+    while(!(UCB0IFG & UCTXIFG0));
+    UCB0TXBUF = data;
+    while(UCB0CTLW0 & UCTXSTP);             // wait for stop
 }
 
 int main(void) {
 
-	char states_right[NUM_OF_STATES] = {0x11, 0x40, 0x19, 0x60}; // 00010001, 01000000, 00011001, 01100000 (PH/EN) + blink led2
-	char states_left[NUM_OF_STATES] = {0x10, 0x40, 0x11, 0x60}; // 00010000, 01000000, 00010001, 01100000
-	char next_state = 0;
-	int steps_to_move = 20;
+    uint8_t states_right[NUM_OF_STATES] = {0x88, 0xA0, 0x89, 0xE0}; // P13 = PA, P14 = DA, P15 = PB, P16 = DB, P17 = SL
+    uint8_t states_left[NUM_OF_STATES] = {0x22, 0x28, 0x26, 0x38};  // P1 = PA, P2 = DA, P3 = PB, P4 = DB, P5 = SL
+    uint8_t next_state = 0;
+    uint8_t steps_to_move = 20;
 
-	init();
+    init();
+    i2c_init();
 
-	while (steps_to_move > 0){
-		if (next_state > (NUM_OF_STATES -1)){
-			next_state = 0;
-		}
-		P1OUT = states_right[next_state];
-		P3OUT = states_left[next_state];
+    // enable TCA9539
+    P1OUT |= BIT4;
 
-		__delay_cycles(160000);
-		// Make all outputs low
-		P1OUT &= ~0x78;
-		P3OUT &= ~0x71;
+    // make all outputs low
+    i2c_transmit(0x02, 0x00);
+    i2c_transmit(0x03, 0x00);
 
-		__delay_cycles(480000);
+    //configure output
+    i2c_transmit(0x05, 0xE1); // ~0x1E
+    i2c_transmit(0x06, 0x0F); // ~0xF0
 
-		next_state++;
-		steps_to_move--;
-	}
+    while (steps_to_move > 0){
+        if (next_state > (NUM_OF_STATES -1)){
+            next_state = 0;
+        }
+        i2c_transmit(0x02, states_left[next_state]);
+        i2c_transmit(0x03, states_right[next_state]);
 
-	return 0;
+        __delay_cycles(160000);
+
+        // Make all outputs low
+        i2c_transmit(0x02, 0x00);
+        i2c_transmit(0x03, 0x00);
+
+        __delay_cycles(480000);
+
+        next_state++;
+        steps_to_move--;
+    }
+
+    return 0;
 }
