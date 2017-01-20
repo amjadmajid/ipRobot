2570764d46653d381b28fc1547b4a54ac5f72680
Koen Schaper <Kpschaper@gmail.com>
2017-01-20 16:20:40 +0100

    Added a function to drive motors for x numer of steps given a directonal command

--- Code/Motor-driver/main.c
+++ Code/Motor-driver/main.c
@@ -7,7 +7,6 @@
 #include <stdint.h>
 #include "eusci_b0_i2c.h"
 
-#define NUM_OF_STATES 4
 #define MCF 8000000 // Hz
 #define FREQ 20
 #define DUTY 0.25
@@ -15,7 +14,6 @@
 // Note: INFOD has a length of 128 bytes
 struct NVvar {
     uint8_t cp;
-    uint8_t state;
     uint8_t cnt;
     uint8_t len;
     uint8_t inst[100];
@@ -46,7 +44,7 @@ void init(void) {
     P3OUT &= ~(BIT4 | BIT5);                // Disable both motor drivers
 }
 
-void motor_init() {
+void drv_init() {
 
     i2c_init();
 
@@ -60,6 +58,70 @@ void motor_init() {
     //configure output
     i2c_transmit(0x06, 0x00);   //0x03 for TCA9538
     _delay_cycles(800);         // 100us delay;
+}
+
+void prep_inst(uint8_t cmd, uint8_t len) {
+
+    const uint8_t lstates[4] = {0x02, 0x08, 0x03, 0x0C};  // P0 = DA, P1 = PA, P2 = DB, P3 = PB
+    const uint8_t rstates[4] = {0x0C, 0x03, 0x08, 0x02};  // P4 = DA, P1 = PA, P2 = DB, P3 = PB
+    uint8_t cnt = 0;
+    int8_t lstate = 0;
+    int8_t rstate = 0;
+    int8_t ladd = 0;
+    int8_t radd = 0;
+
+
+    switch(cmd) {
+       case 0x01 : // forward
+           ladd = 1;
+           radd = 1;
+           break;
+       case 0x02 : // left (hard)
+           ladd = -1;
+           radd = 1;
+           break;
+       case 0x03 : // right (hard)
+           ladd = 1;
+           radd = -1;
+           break;
+       case 0x04 : // reverse
+           ladd = -1;
+           radd = -1;
+           break;
+       /* case 0x05 : // left (soft forward)
+
+           break;
+       case 0x06 : // right (soft forward)
+
+           break;
+       case 0x07 : // left (soft reverse)
+
+           break
+       case 0x08 : // right (soft reverse)
+
+           break;
+       default : */
+
+    }
+
+    while(cnt < len) {
+        lstate = lstate + ladd;
+        rstate = rstate + radd;
+        if (lstate > 3) {
+            lstate = 0;
+        } else if (lstate < 0) {
+            lstate = 3;
+        }
+        if (rstate > 3) {
+            rstate = 0;
+        } else if (rstate < 0) {
+            rstate = 3;
+        }
+        fram->inst[cnt] = (lstates[lstate] | (rstates[rstate] << 4));
+        cnt++;
+    }
+    fram->len = len;
+    fram->cp++;
 }
 
 void drive_motors() {
@@ -85,33 +147,18 @@ void drive_motors() {
 
 int main(void) {
 
-    const uint8_t states_left[NUM_OF_STATES] = {0x02, 0x08, 0x03, 0x0C};  // P0 = DA, P1 = PA, P4 = DB, P3 = PB
-    const uint8_t states_right[NUM_OF_STATES] = {0x0C, 0x03, 0x08, 0x02}; // P4 = DA, P5 = PA, P6 = DB, P7 = PB
     const uint16_t stm = 20;
 
     if( fram->cp == 0x00 ){
         fram->cnt = 0;
-        fram->state = 0;
         fram->cp = 0x01;
     }
 
     init();
-    motor_init();
-
-    //prepaire array
+    drv_init();
 
     if( fram->cp == 0x01 ) {
-        fram->len = stm;
-        while(fram->cnt < fram->len) {
-            if (fram->state > (NUM_OF_STATES -1)){
-                fram->state = 0;
-            }
-            fram->inst[fram->cnt] = (states_left[fram->state] | (states_right[fram->state] << 4));
-            fram->state++;
-            fram->cnt++;
-        }
-        fram->cp++;
-        fram->cnt = 0;
+        prep_inst(0x01, stm);
     }
 
     drive_motors();
