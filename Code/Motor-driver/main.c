e0ab82de468f0c2fe0a573a13d74002ce40916a8
Koen Schaper <Kpschaper@gmail.com>
2017-01-20 11:01:06 +0100

    Separate function for driving the motors. First perpare a array with instructions, then drive the motors.

--- Code/Motor-driver/main.c
+++ Code/Motor-driver/main.c
@@ -12,11 +12,16 @@
 #define FREQ 20
 #define DUTY 0.25
 
+// Note: INFOD has a length of 128 bytes
 struct NVvar {
-    uint8_t started;
-    uint16_t cnt;
-    uint8_t next_state;
+    uint8_t cp;
+    uint8_t state;
+    uint8_t cnt;
+    uint8_t len;
+    uint8_t inst[100];
 };
+
+struct NVvar * fram = (struct NVvar *) 0x1800;
 
 void init(void) {
 
@@ -41,22 +46,37 @@ void init(void) {
     P3OUT &= ~(BIT4 | BIT5);                // Disable both motor drivers
 }
 
+void drive_motors() {
+
+    while (fram->cnt < fram->len) {
+
+        P4OUT |= BIT0;
+        // enable both motor drivers
+        P3OUT |= (BIT4 | BIT5);
+        i2c_transmit(0x02, fram->inst[fram->cnt]); //0x01 for TCA9538
+
+        __delay_cycles((MCF/FREQ) * DUTY);
+        fram->cnt++;
+
+        P4OUT &= ~BIT0;
+        i2c_transmit(0x02, 0x00); //0x01 for TCA9538
+        // disable both motor drivers
+        P3OUT &= ~(BIT4 | BIT5);
+
+        __delay_cycles((MCF/FREQ) * (1-DUTY));
+    }
+}
+
 int main(void) {
 
-    struct NVvar * fram = (struct NVvar *) 0x1800;
     const uint8_t states_left[NUM_OF_STATES] = {0x02, 0x08, 0x03, 0x0C};  // P0 = DA, P1 = PA, P4 = DB, P3 = PB
     const uint8_t states_right[NUM_OF_STATES] = {0x0C, 0x03, 0x08, 0x02}; // P4 = DA, P5 = PA, P6 = DB, P7 = PB
-    const uint16_t steps_to_move = 20;
-    uint16_t cnt;
-    uint8_t next_state;
+    const uint16_t stm = 20;
 
-    if( fram->started != 0x02 ){
-        cnt = 0;
-        next_state = 0;
-        fram->started = 0x02;
-    } else{
-        cnt = fram->cnt;
-        next_state = fram->next_state;
+    if( fram->cp == 0x00 ){
+        fram->cnt = 0;
+        fram->state = 0;
+        fram->cp = 0x01;
     }
 
     init();
@@ -73,28 +93,25 @@ int main(void) {
     i2c_transmit(0x06, 0x00);   //0x03 for TCA9538
     _delay_cycles(800);         // 100us delay;
 
-    while (cnt < steps_to_move) {
-        if (next_state > (NUM_OF_STATES -1)){
-            next_state = 0;
+    //prepaire array
+
+    if( fram->cp == 0x01 ) {
+        fram->len = stm;
+        while(fram->cnt < fram->len) {
+            if (fram->state > (NUM_OF_STATES -1)){
+                fram->state = 0;
+            }
+            fram->inst[fram->cnt] = (states_left[fram->state] | (states_right[fram->state] << 4));
+            fram->state++;
+            fram->cnt++;
         }
-        P4OUT |= BIT0;
-        // enable both motor drivers
-        P3OUT |= (BIT4 | BIT5);
-        i2c_transmit(0x02, (states_left[next_state] | (states_right[next_state] << 4))); //0x01 for TCA9538
-
-        __delay_cycles((MCF/FREQ) * DUTY);
-
-        P4OUT &= ~BIT0;
-        i2c_transmit(0x02, 0x00); //0x01 for TCA9538
-        // disable both motor drivers
-        P3OUT &= ~(BIT4 | BIT5);
-
-        fram->next_state = ++next_state;
-        fram->cnt = ++cnt;
-
-        __delay_cycles((MCF/FREQ) * (1-DUTY));
+        fram->cp++;
+        fram->cnt = 0;
     }
-    fram->started = 0x00;
+
+    drive_motors();
+
+    fram->cp = 0x00;
 
     return 0;
 }
