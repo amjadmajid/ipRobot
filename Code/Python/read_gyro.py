771980e1d6ab18361c5b9c2913d67c27700d227b
Koen Schaper <Kpschaper@gmail.com>
2017-08-02 16:05:32 +0200

    Show error when debugger is already used by code composer!

--- Code/Python/read_gyro.py
+++ Code/Python/read_gyro.py
@@ -16,7 +16,12 @@ process = subprocess.Popen(
     ["MSP430Flasher.exe", "-g", "-r", "[output.hex,0x4400-" + str(end) + "]", "-z", "[VCC=2200]"],
     stdin=subprocess.PIPE)
 
+# Do not update debugger firmware!
 process.communicate("n\n")
+
+if process.returncode != 0:
+    print "\n ERROR: The debugger is already in use!"
+    quit(1)
 
 output = open('output.hex', 'r')
 arr = output.readlines()
