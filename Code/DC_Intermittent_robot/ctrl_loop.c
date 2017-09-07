384f3653ab2880d435c0170df1136c55b09c4bc1
Koen Schaper <Kpschaper@gmail.com>
2017-09-07 15:00:58 +0200

    Separate function for computing the pid: http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-sample-time/

--- Code/DC_Intermittent_robot/ctrl_loop.c
+++ Code/DC_Intermittent_robot/ctrl_loop.c
@@ -14,24 +14,26 @@
 #include "motor_ctrl.h"
 
 // Ziegler Nichols PID tuning parameters
-#define Ku 0.6
-#define Tu 0.24 // 12/50
+#define ku 0.6
+#define tu 0.24 // 12/50
 #define st 0.02 // Sample time (50Hz)
 
 #pragma PERSISTENT(sensor_data);
 int16_t sensor_data[400] = {0};
 
-uint16_t lspeed = 600;
-uint16_t rspeed = 600;
+uint16_t lspeed = 250;
+uint16_t rspeed = 250;
 
-float Kp = 0.6 * Ku;
-float Ki = 0; //(Tu / 2) * st; // (Tu / 1.2) * st;
-float Kd = 0; //(Tu / 8) / st;
+float kp = 0.6 * ku;
+float ki = 0; //(tu / 2); // (tu / 1.2);
+float kd = 0; //(tu / 8);
 
-int16_t data = 0;
-float set = 0, omega = 0, err = 0;
-float ierr = 0;
-float prev = 0, derr = 0;
+float out_max = 250, out_min = -250;
+
+float ang = 0;
+float set = 0;
+float iterm = 0;
+float prev = 0;
 
 void ctrl_init(){
 
@@ -39,7 +41,7 @@ void ctrl_init(){
     gyro_init();
     drv_init();
 
-    TA2CCR0 = 188;                             // Set timer to approx. 50Hz
+    TA2CCR0 = 188;                            // Set timer to approx. 50Hz
     TA2CTL = TASSEL__ACLK | MC__UP;           // ACLK = VLO, UP mode
 
     __bis_SR_register(GIE);                   // Enable interrupt
@@ -51,15 +53,43 @@ void enbl_loop(uint16_t dur){
     TA2CCTL0 = CCIE;                          // TACCR0 interrupt enabled
 }
 
-void upd_set(float s){
-    set = s;
-}
-
 void dsbl_loop(){
     TA2CCTL0 &= ~CCIE;
     dsbl_mot();
     set = 0;                                  // Always return set to 0 (straight)
 }
+
+// allow changing of tuning parameters for the different commands
+void set_tunings(float Kp, float Ki, float Kd){
+    kp = Kp;
+    ki = Ki * st;
+    kd = Kd / st;
+}
+
+void set_limits(float max, float min){
+    out_max = max;
+    out_min = min;
+}
+
+// compute PID output
+float pid_compute(float input){
+    float err, dinp, output;
+    err = set - input;
+    iterm += (ki * err);
+    if(iterm > out_max)
+        iterm = out_max;
+    else if(iterm < out_min)
+        iterm = out_min;
+    dinp = input - prev;
+    output = kp*err + iterm + kd*dinp;
+    if(output > out_max)
+        output = out_max;
+    else if(output < out_min)
+        output = out_min;
+    prev = input;
+    return output;
+}
+
 
 // Timer2_A0 interrupt service routine
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
@@ -71,18 +101,13 @@ void __attribute__ ((interrupt(TIMER2_A0_VECTOR))) Timer2_A0_ISR (void)
 #error Compiler not supported!
 #endif
 {
-    data = -gyro_read();
+    int16_t data;
+    float omega;
+    data = gyro_read();
     omega = data / 131.0;
-    err = set - omega;
-    ierr += err;
-    derr = err - prev;
-    if(fram.cnt < 100 && RAMP){
-        lspeed += 5;
-        rspeed += 5;
-    }
-    rspeed = rspeed - (Kp*err + Ki*ierr + Kd*derr);
+    ang += omega * st; // integrate to get the angle
+    rspeed = rspeed - (int16_t)pid_compute(ang);
     set_for_sp(lspeed, rspeed);
     sensor_data[fram.cnt] = data;
-    prev = err;
     fram.cnt++;
 }
