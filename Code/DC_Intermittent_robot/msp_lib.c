013dd180cf0be5e209429a8e3ca1792f7f9daff2
Koen Schaper <Kpschaper@gmail.com>
2017-09-30 09:30:46 +0200

    Modified random number generator to use timer A3

--- Code/DC_Intermittent_robot/msp_lib.c
+++ Code/DC_Intermittent_robot/msp_lib.c
@@ -12,7 +12,7 @@
 /*
 * Based on the random number generator example:
 * http://processors.wiki.ti.com/index.php/MSP430_FAQ#How_to_generate_random_number_with_MSP430_devices.3F
-* Modified to work with MSP430FR59xx
+* Modified to work with MSP430FR5969 with timer A3 where ACLK is connected to CCI0B
 * Only used to generate a seed value
 */
 uint16_t get_seed() {
@@ -20,26 +20,26 @@ uint16_t get_seed() {
     uint8_t i;
     uint16_t result = 0;
 
-    // Timer0_A3 Setup
-    TA0CCTL2 = CM_1 + CCIS_1 + CAP;
-    TA0CTL |= TASSEL__SMCLK + MC__CONTINOUS + TACLR;
+    // Timer_A3
+    TA3CCTL0 = CM_1 + CCIS_1 + CAP;
+    TA3CTL |= TASSEL__SMCLK + MC__CONTINOUS + TACLR;
 
     for(i=0 ; i<16 ; i++) {
 
         // shift left result
         result <<= 1;
         // wait until Capture flag is set
-        while(!(TA0CCTL2 & CCIFG));
+        while(!(TA3CCTL0 & CCIFG));
         // clear flag
-        TA0CCTL2 &= ~CCIFG;
+        TA3CCTL0 &= ~CCIFG;
         // check LSB
-        if(TA0CCR2 & 0x01) {
+        if(TA3CCR0 & 0x01) {
             result |= 0x01;
         }
         // change the division of timer input clock
-        TA0CTL = (TA0CTL & 0xFF3F) | ((TA0CCR2 & 0x03) << 6);
+        TA3CTL = (TA3CTL & 0xFF3F) | ((TA3CCR0 & 0x03) << 6);
     }
-    //disable timer
-    TA0CTL = MC_0;
+    // stop timer
+    TA3CTL = MC__STOP;
     return result;
 }
