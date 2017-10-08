a6143743bb32fe27f9da85bd073199f5740c133e
Koen Schaper <Kpschaper@gmail.com>
2017-10-08 13:26:38 +0200

    Save experiment data!

--- Code/Python/Experiments/distance_bat_vs_ipr_06_10.py
+++ Code/Python/Experiments/distance_bat_vs_ipr_06_10.py
@@ -5,7 +5,12 @@ set_dist = [50, 100, 150, 200]
 
 bat_dist = [[50.9, 51.1, 50.3], [102.3, 102.6, 104.2], [156.1, 156.7, 157.1], [213.2, 214.8, 215.0]]
 
-int_bat_dist = [[49.5, 49.6, 50.2], [98.4, 98.9, 99.5], [148.3, 147.6, 148.1], [191.4, 194.2, 194.8]]
+int_1100_bat_dist = [[49.5, 49.6, 50.2], [98.4, 98.9, 99.5], [148.3, 147.6, 148.1], [191.4, 194.2, 194.8]]
+
+int_825_bat_dist = [[45.1, 45, 45.1], [91.2, 93.4, 91.4], [141.1, 137.3, 136.2], [183.6, 183.4, 185.1]]
+
+int_550_bat_dist = [[41.6, 40.1, 41.5], [85, 85.1, 86.8], [127.3, 125.8, 128.1], [175.2, 175.6, 172.9]]
+
 
 def list_avg(llist):
     i = 0
@@ -17,12 +22,14 @@ def list_avg(llist):
 
 plt.figure(1)
 plt.plot(set_dist, list_avg(bat_dist))
-plt.plot(set_dist, list_avg(int_bat_dist))
+plt.plot(set_dist, list_avg(int_1100_bat_dist))
+plt.plot(set_dist, list_avg(int_825_bat_dist))
+plt.plot(set_dist, list_avg(int_550_bat_dist))
 plt.xlim(xmin=0)  # this line
 plt.ylim(ymin=0)  # this line
 plt.grid()
 plt.xlabel('set distance (cm)')
 plt.ylabel('measured distance (cm)')
-plt.legend(('Battery powered', 'BP with a software interrupt every 1.1 sec'))
+plt.legend(('Battery powered', 'BP with a software interrupt every 1.1 sec', 'BP with a software interrupt every 0.825 sec', 'BP with a software interrupt every 0.55 sec'))
 
 plt.show()
---
+++ Code/Python/Experiments/intermittent_weight_experiment_07-10.py
@@ -0,0 +1,31 @@
+from __future__ import division
+import matplotlib.pyplot as plt
+
+set_dist = [50, 100, 150, 200]
+
+int_1100_bat_dist = [[49.5, 49.6, 50.2], [98.4, 98.9, 99.5], [148.3, 147.6, 148.1], [191.4, 194.2, 194.8]]
+
+dist_10g = [[41.1, 41.4, 40.9], [80.9, 80.5, 79.7], [125.2, 125.8, 123.3], [172.8, 166.6, 167.9]]
+
+dist_20g = [[33.8, 35.7, 34.6], [68.4, 70.3, 68.3], [101.5, 104.1, 102.1], [142.3, 145.4, 140.8]]
+
+def list_avg(llist):
+    i = 0
+    avg = []
+    while i < len(llist):
+        avg.append(sum(llist[i])/len(llist[i]))
+        i+=1
+    return avg
+
+plt.figure(1)
+plt.plot(set_dist, list_avg(int_1100_bat_dist))
+plt.plot(set_dist, list_avg(dist_10g))
+plt.plot(set_dist, list_avg(dist_20g))
+plt.xlim(xmin=0)  # this line
+plt.ylim(ymin=0)  # this line
+plt.grid()
+plt.xlabel('set distance (cm)')
+plt.ylabel('measured distance (cm)')
+plt.legend(('No weight added', '10 gram added', '20 gram added'))
+
+plt.show()
