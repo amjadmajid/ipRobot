d4fec67a8e7ef32c6bb15bbb368f4d4d6c947781
Koen Schaper <Kpschaper@gmail.com>
2017-11-07 17:47:36 +0100

    Update robot tracking

--- Code/Python/Video experiments/robot_tracking.py
+++ Code/Python/Video experiments/robot_tracking.py
@@ -8,20 +8,10 @@ import argparse
 import imutils
 import cv2
 
-# construct the argument parse and parse the arguments
-"""
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-                help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=256,
-                help="max buffer size")
-args = vars(ap.parse_args())
-"""
-
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-greenLower = (24, 100, 100)
+greenLower = (24, 100, 50)
 greenUpper = (64, 255, 255)
 
 dir = 'Video Circle/Video_r1_right_r30_battery/'
