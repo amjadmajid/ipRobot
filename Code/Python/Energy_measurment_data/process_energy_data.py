6838d0c53ba1480b1c4b951c6378597e3fcc52d3
Koen Schaper <Kpschaper@gmail.com>
2017-11-07 12:23:04 +0100

    Mearsure led power consumption as well

--- Code/Python/Energy_measurment_data/process_energy_data.py
+++ Code/Python/Energy_measurment_data/process_energy_data.py
@@ -5,24 +5,39 @@ import numpy as np
 import pandas as pd
 
 
-def chop_ends(dlist, thres):
-    dlist = map(float, dlist)
-    # Remove "zero" data
-    i = 0
-    while dlist[i] < thres:
-        i+=1
-    while dlist[i] > thres:
-        i+=1
-    #print 'i:' + str(i)
-    dlist = dlist[i:]
-    j = len(dlist)-1
-    while dlist[j] > thres:
-        j-=1
-    #print 'j:' + str(j)
-    dlist = dlist[:j]
+def chop_ends(dlist, thres, inv):
+    if inv != 1:
+        dlist = map(float, dlist)
+        # Remove "zero" data
+        i = 0
+        while dlist[i] < thres:
+            i+=1
+        while dlist[i] > thres:
+            i+=1
+        #print 'i:' + str(i)
+        dlist = dlist[i:]
+        j = len(dlist)-1
+        while dlist[j] > thres:
+            j-=1
+        #print 'j:' + str(j)
+        dlist = dlist[:j]
+    else:
+        dlist = map(float, dlist)
+        # Remove "zero" data
+        i = 0
+        while dlist[i] < thres:
+            i+=1
+        #print 'i:' + str(i)
+        dlist = dlist[i:]
+        j = len(dlist)-1
+        while dlist[j] < thres:
+            j-=1
+        #print 'j:' + str(j)
+        dlist = dlist[:j]
     return dlist
 
-def get_max_avg_cur(file):
+
+def get_max_avg_cur(file, inv):
     with open(file) as f_obj:
         """
         Read a CSV file using csv.DictReader
@@ -38,7 +53,7 @@ def get_max_avg_cur(file):
              avg_vol.append(line["Main Avg Voltage (V)"])
              max_cur.append(line["Main Max Current (mA)"])
 
-    avg_cur = chop_ends(avg_cur, 3)
+    avg_cur = chop_ends(avg_cur, 3, inv)
 
     ws = 100
     avgc = pd.Series(avg_cur).rolling(ws).mean().dropna().tolist()
@@ -52,15 +67,23 @@ def get_max_avg_cur(file):
     plt.ylabel("Current (mA)")
 
 plt.figure(1)
-get_max_avg_cur("Wisp_Enable_8Mhz_to_LED.csv")
+get_max_avg_cur("Wisp_Enable_8Mhz_to_LED.csv", 0)
 plt.figure(2)
-get_max_avg_cur("R1_Wisp_Enable_8Mhz_to_LED.csv")
+get_max_avg_cur("R1_Wisp_Enable_8Mhz_to_LED.csv", 0)
 plt.figure(3)
-get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_gyro_to_LED.csv")
+get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_gyro_to_LED.csv", 0)
 plt.figure(4)
-get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_proximity_to_LED.csv")
+get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_proximity_to_LED.csv", 0)
 plt.figure(5)
-get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_hbridge_to_LED.csv")
+get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_hbridge_to_LED.csv", 0)
 plt.figure(6)
-get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_gyro_and_hbridge_to_LED.csv")
+get_max_avg_cur("R1_Wisp_Enable_8Mhz_and_gyro_and_hbridge_to_LED.csv", 0)
+
+plt.figure(7)
+get_max_avg_cur("Wisp_Enable_8Mhz_to_Enable_green_LED(wisp5.0).csv", 1)
+plt.figure(8)
+get_max_avg_cur("Wisp_Enable_8Mhz_to_Enable_red_LED(wisp5.1).csv", 1)
+
 plt.show()
+
+
