99d5dec79d8a9ae7bc92711bf279a44fc374b524
Koen Schaper <Kpschaper@gmail.com>
2017-11-10 11:26:35 +0100

    Convert pixel data to cm and plot in matplotlib!

--- Code/Python/Video experiments/robot_tracking.py
+++ Code/Python/Video experiments/robot_tracking.py
@@ -2,11 +2,13 @@
 # https://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/
 
 # import the necessary packages
+from __future__ import division
 from collections import deque
 import numpy as np
 import argparse
 import imutils
 import cv2
+import matplotlib.pyplot as plt
 
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
@@ -16,8 +18,10 @@ greenUpper = (64, 255, 255)
 
 dir = 'Video Circle/Video_r1_right_r30_battery/'
 file = dir + 'DSC_4781.MOV'
-buffer = 25600
 line_color = (0, 0, 255)
+
+rect_p1 = (440, 40)
+rect_p2 = (1460, 1040)
 
 camera = cv2.VideoCapture(file)
 
@@ -96,7 +100,7 @@ while cnt < num_frames:
     cnt += 1
 
 print "Finished"
-cv2.rectangle(frame, (440, 40), (1460, 1040), (0, 0, 255), 2)
+cv2.rectangle(frame, rect_p1, rect_p2, (0, 0, 255), 2)
 cv2.imshow("Frame", frame)
 # wait for any keypress
 cv2.waitKey(0)
@@ -104,3 +108,28 @@ cv2.waitKey(0)
 # cleanup the camera and close any open windows
 camera.release()
 cv2.destroyAllWindows()
+
+list = list(pts)
+clist = [i for i in list if i is not None]
+
+x = rect_p2[0] - rect_p1[0]
+y = rect_p2[1] - rect_p1[1]
+
+x2 = (1920 - x)/2
+y2 = (1080 - y)/2
+
+# scale circle acording to square dimensions in cm
+slist = []
+for i in clist:
+    x3 = ((i[0] - x2) * (80 / x))
+    y3 = ((i[1] - y2) * (80 / y))
+    # inverse y while images start in the top left corner instead of bottom left
+    y3 = 80 - y3
+    slist.append((x3, y3))
+
+print clist
+print slist
+plt.figure(1)
+plt.plot(*zip(*slist))
+plt.axes().set_aspect('equal', 'datalim')
+plt.show()
