e908f4508f973ba0a03626772504f30882a5f4d2
Koen Schaper <Kpschaper@gmail.com>
2017-11-11 10:11:19 +0100

    Update gitignore to only ignore video folders and modified robot_tracking.py to save data in csv

--- Code/Python/Video experiments/robot_tracking.py
+++ Code/Python/Video experiments/robot_tracking.py
@@ -3,14 +3,15 @@
 
 # import the necessary packages
 from __future__ import division
+import os
+import glob
 from collections import deque
-import numpy as np
-import argparse
 import imutils
 import cv2
 import matplotlib.pyplot as plt
+import csv
 
-live = 1
+live = 0
 
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
@@ -18,123 +19,131 @@ live = 1
 greenLower = (29, 100, 50)
 greenUpper = (64, 255, 255)
 
-dir = 'Video Circle/Video_r1_right_r30_battery/'
-file = dir + 'DSC_4781.MOV'
 line_color = (0, 0, 255)
 
 rect_p1 = (440, 40)
 rect_p2 = (1460, 1040)
 
-camera = cv2.VideoCapture(file)
+square_size = 80
 
-# Get total number of frames for OpenCV 3+
-num_frames = int(camera.get(cv2.CAP_PROP_FRAME_COUNT))
+ddir = 'Csv_Data/'
 
-pts = deque(maxlen=num_frames)
+mdir = 'Video_Square/'
+sdir = 'Video_trg_50_r1_50_right_battery_int750'
+#file = mdir + sdir + '/' + 'DSC_4945.MOV'
 
-cnt = 0
-# keep looping
-while cnt < num_frames:
-    # grab the current frame
-    (grabbed, frame) = camera.read()
+fcnt = 1
+for file in glob.glob(os.path.join(mdir+sdir+'/', '*.MOV')):
+    print 'Round: ' + str(fcnt)
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    camera = cv2.VideoCapture(file)
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    # Get total number of frames for OpenCV 3+
+    num_frames = int(camera.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
+    pts = deque(maxlen=num_frames)
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    lcnt = 0
+    # keep looping
+    while lcnt < num_frames:
+        # grab the current frame
+        (grabbed, frame) = camera.read()
 
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                       (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        # resize the frame, blur it, and convert it to the HSV
+        # color space
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # update the points queue
-    pts.appendleft(center)
+        # construct a mask for the color "green", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, greenLower, greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
-    # loop over the set of tracked points
-    for i in xrange(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
 
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        #thickness = int(np.sqrt(buffer / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], line_color, 2)
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-    if live:
-        # show the frame to our screen
-        cv2.imshow("Frame", frame)
-        #cv2.imshow('mask', mask)
-        key = cv2.waitKey(1) & 0xFF
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-        # if the 'q' key is pressed, stop the loop
-        if key == ord("q"):
-            break
+        # update the points queue
+        pts.appendleft(center)
 
+        # loop over the set of tracked points
+        for i in xrange(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
 
-    cnt += 1
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            #thickness = int(np.sqrt(buffer / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], line_color, 2)
 
-print "Finished"
-cv2.rectangle(frame, rect_p1, rect_p2, (0, 0, 255), 2)
-cv2.imshow("Frame", frame)
-# wait for any keypress
-cv2.waitKey(0)
+        if live:
+            # show the frame to our screen
+            cv2.imshow("Frame", frame)
+            #cv2.imshow('mask', mask)
+            key = cv2.waitKey(1) & 0xFF
 
-# cleanup the camera and close any open windows
-camera.release()
-cv2.destroyAllWindows()
+            # if the 'q' key is pressed, stop the loop
+            if key == ord("q"):
+                break
 
-if not live:
-    list = list(pts)
-    clist = [i for i in list if i is not None]
+        lcnt += 1
 
-    x = rect_p2[0] - rect_p1[0]
-    y = rect_p2[1] - rect_p1[1]
+    print "Finished"
+    cv2.rectangle(frame, rect_p1, rect_p2, (0, 0, 255), 2)
+    cv2.imshow("Frame", frame)
+    # wait for any keypress
+    cv2.waitKey(0)
 
-    x2 = (1920 - x)/2
-    y2 = (1080 - y)/2
+    # cleanup the camera and close any open windows
+    camera.release()
+    cv2.destroyAllWindows()
 
-    # scale circle acording to square dimensions in cm
-    slist = []
-    for i in clist:
-        x3 = ((i[0] - x2) * (80 / x))
-        y3 = ((i[1] - y2) * (80 / y))
-        # inverse y while images start in the top left corner instead of bottom left
-        y3 = 80 - y3
-        slist.append((x3, y3))
+    if not live:
+        plist = list(pts)
+        clist = [i for i in plist if i is not None]
 
-    print clist
-    print slist
-    plt.figure(1)
-    plt.plot(*zip(*slist))
-    plt.xlim(xmax=80)
-    plt.ylim(ymax=80)
-    plt.axes().set_aspect('equal', 'datalim')
-    plt.show()
+        x = rect_p2[0] - rect_p1[0]
+        y = rect_p2[1] - rect_p1[1]
+
+        x2 = (1920 - x)/2
+        y2 = (1080 - y)/2
+
+        # scale circle acording to square dimensions in cm
+        slist = []
+        for i in clist:
+            x3 = ((i[0] - x2) * (square_size / x))
+            y3 = ((i[1] - y2) * (square_size / y))
+            # inverse y while images start in the top left corner instead of bottom left
+            y3 = square_size - y3
+            slist.append((x3, y3))
+
+        # Write to csv!
+        with open(ddir + mdir + sdir + '_' + str(fcnt) + '.csv','wb') as out:
+            csv_out=csv.writer(out)
+            csv_out.writerow(['x','y'])
+            for row in slist:
+                csv_out.writerow(row)
+    fcnt += 1
