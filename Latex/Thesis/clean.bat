7c122cee304cc593c56f9e22b5a16c1cd5c9dd86
Koen Schaper <Kpschaper@gmail.com>
2017-11-21 08:37:20 +0100

    Update transiently powered systems!

---
+++ Latex/Thesis/clean.bat
@@ -0,0 +1,10 @@
+set TARGET=thesis
+del /s /q /f *.aux
+del /s /q /f *.log
+del /s /q /f *.bak 
+del /s /q /f %TARGET%.dvi 
+del /s /q /f %TARGET%.pdf 
+del /s /q /f %TARGET%.toc 
+del /s /q /f %TARGET%.bbl 
+del /s /q /f %TARGET%.blg
+del /s /q /f %TARGET%.out
