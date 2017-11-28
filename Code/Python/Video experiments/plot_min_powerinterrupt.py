6eaf810aa0a15e70362e48aeaf8521394034b168
Koen Schaper <Kpschaper@gmail.com>
2017-11-28 15:14:47 +0100

    Upload updated plot files!

--- Code/Python/Video experiments/plot_min_powerinterrupt.py
+++ Code/Python/Video experiments/plot_min_powerinterrupt.py
@@ -56,7 +56,8 @@ def plt_distance_from_ref_circle(data_list):
 
 def plot_raw_data(data_list):
 
-    plt.figure()
+    plt.figure(figsize=(3, 5))
+    plt.subplots_adjust(left=0.2)
     for dlist in data_list:
         plt.plot(*zip(*dlist[0]), label=label[dlist[1]], color=color_list[dlist[1]], marker=marker_list[dlist[1]], markevery=20, alpha=0.5)
 
@@ -69,14 +70,14 @@ def plot_raw_data(data_list):
         plt.vlines(x=39, ymin=1, ymax=76, color='black', linewidth=2)
         plt.hlines(y=1, xmin=32, xmax=46, color='black', linewidth=2)
         plt.hlines(y=76, xmin=32, xmax=46, color='black', linewidth=2)
-    plt.xlim(xmin=-10, xmax=90)
+    plt.xlim(xmin=20, xmax=80)
     plt.ylim(ymin=-10, ymax=90)
     # plt.title(file_name)
     plt.xlabel('x distance in cm', fontsize=12)
     plt.ylabel('y distance in cm', fontsize=12)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
-    plt.axes().set_aspect('equal', 'datalim')
+    #plt.axes().set_aspect('equal', 'datalim')
     # remove duplicate labels
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = OrderedDict(zip(labels, handles))
@@ -84,35 +85,19 @@ def plot_raw_data(data_list):
     plt.show()
 
 
-
-
 ddir = 'Csv_Data/'
 
-movement = 1
-set = 0
+movement = 4
+set = 2
 pwm = ['30', '50', '70']
-re_list = ['', '_int500', '_int750', '_int1000', '_int1250']
-if movement == 1:
-    mdir = 'Video_Circle/'
-    file_name = 'Video_trg_' + pwm[set] + '_r1_right_r30_battery'
-elif movement == 2:
-    mdir = 'Video_Square/'
-    file_name = 'Video_trg_70_r1_right_battery'
-elif movement == 3:
-    mdir = 'Video_Straight/'
-    file_name = 'Video_trg_' + pwm[set] + '_r1_75_battery'
-elif movement == 4:
-    mdir = 'Video_Straight_4s/'
-    file_name = 'Video_trg_' + pwm[set] + '_r1_battery'
-    re_list = ['_int200', '_int300', '_int400']
+mdir = 'Video_Straight_4s/'
+file_name = 'Video_trg_' + pwm[set] + '_r1_battery'
+re_list = ['_int200', '_int300', '_int400']
 
 csv_list = os.listdir(ddir+mdir)
 csv_list.sort(key=natural_keys)
 
-if movement != 4:
-    label = ['No Interrupt', 'Interrupt 0.5s', 'Interrupt 0.75s', 'Interrupt 1.0s', 'Interrupt 1.25s']
-else:
-    label = ['Interrupt 0.2s', 'Interrupt 0.3s', 'Interrupt 0.4s']
+label = ['Interrupt 0.2s', 'Interrupt 0.3s', 'Interrupt 0.4s']
 color_list = ['blue', 'orange', 'green', 'red', 'cyan']
 marker_list = ['o', '^', 's', '+', 'x']
 ls_list = [':', '-.', '--', '-']
@@ -128,7 +113,7 @@ for dlist in data_list:
         #print 'avg ang of movement:' + str(avg_ang_of_straight(dlist[0]))
 
 
-#if movement == 1:
-    #plt_distance_from_ref_circle(data_list)
+if movement == 1:
+    plt_distance_from_ref_circle(data_list)
 
 plot_raw_data(data_list)
